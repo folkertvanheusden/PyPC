bf3e717bf6356a618c2206f324ee8693c9306a5a
Folkert van Heusden <folkert@vanheusden.com>
2025-11-06 22:38:21 +0100

    try

--- main.py
+++ main.py
@@ -26,44 +26,48 @@ if status == 0:
 if status == 1:
     print("GIL is disabled")
 
-devices = []
-devices.append(i8253.i8253())
-kb = keyboard.Keyboard()
-devices.append(kb)
-devices.append(i8255.i8255(kb))
-#scr = mda.MDA()
-scr = cga.CGA(False)
-devices.append(scr)
-devices.append(xtide.XTIDE(('harddisk.img',)));
+try:
+    devices = []
+    devices.append(i8253.i8253())
+    kb = keyboard.Keyboard()
+    devices.append(kb)
+    devices.append(i8255.i8255(kb))
+    #scr = mda.MDA()
+    scr = cga.CGA(False)
+    devices.append(scr)
+    devices.append(xtide.XTIDE(('harddisk.img',)));
 
-roms = []
-roms.append(rom.Rom('roms/GLABIOS.ROM', 0xf000 * 16 + 0xe000))
-roms.append(rom.Rom('roms/ide_xt.bin', 0xd000 * 16 + 0x0000))
+    roms = []
+    roms.append(rom.Rom('roms/GLABIOS.ROM', 0xf000 * 16 + 0xe000))
+    roms.append(rom.Rom('roms/ide_xt.bin', 0xd000 * 16 + 0x0000))
 
-b = bus.Bus(1024 * 1024, devices, roms)
-p = i8088.i8088(b, devices, True)
-state = p.GetState()
-state.SetCS(0xf000)
-state.SetIP(0xfff0)
+    b = bus.Bus(1024 * 1024, devices, roms)
+    p = i8088.i8088(b, devices, True)
+    state = p.GetState()
+    state.SetCS(0xf000)
+    state.SetIP(0xfff0)
 
-t = telnet.Telnet(2300, kb, scr)
-v = vncserver.VNCServer(scr, kb, 5902, True)
+    t = telnet.Telnet(2300, kb, scr)
+    v = vncserver.VNCServer(scr, kb, 5902, True)
 
-print('Use: "telnet localhost 2300" to interact with the emulated system')
-print('and/or connect using a VNC client to localhost:5902 (preferred)')
+    print('Use: "telnet localhost 2300" to interact with the emulated system')
+    print('and/or connect using a VNC client to localhost:5902 (preferred)')
 
-p_time = time.time()
-p_cycles = 0
-while True:
-    # print(f'{state.GetCS():04x}:{state.GetIP():04x} {GetRegisters(state)}')
-    rc = p.Tick()
-    if rc == -1:
-        break
-    cur_cycles = state.GetClock()
-    c_diff = cur_cycles - p_cycles
-    if c_diff >= 4700000:
-        p_cycles = cur_cycles
-        now = time.time()
-        ##print(f'\033[1;82H{100 * c_diff / (now - p_time) / 4700000:.2f}%', end='')
-        print(f'{100 * c_diff / (now - p_time) / 4700000:.2f}%')
-        p_time = now
+    p_time = time.time()
+    p_cycles = 0
+    while True:
+        # print(f'{state.GetCS():04x}:{state.GetIP():04x} {GetRegisters(state)}')
+        rc = p.Tick()
+        if rc == -1:
+            break
+        cur_cycles = state.GetClock()
+        c_diff = cur_cycles - p_cycles
+        if c_diff >= 4700000:
+            p_cycles = cur_cycles
+            now = time.time()
+            ##print(f'\033[1;82H{100 * c_diff / (now - p_time) / 4700000:.2f}%', end='')
+            print(f'{100 * c_diff / (now - p_time) / 4700000:.2f}%')
+            p_time = now
+
+except KeyboardInterrupt as ki:
+    print('exit')
